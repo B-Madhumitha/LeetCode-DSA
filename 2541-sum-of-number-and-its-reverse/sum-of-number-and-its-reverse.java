e7bf1f2509a087fde8e62a422f5e3e569f9ea8ed
B-Madhumitha <madhubgoud20@gmail.com>
2025-07-30 22:31:34 +0530

    Time: 153 ms (27.88%) | Memory: 40.6 MB (76.36%) - LeetSync

---
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int reverse(int n){
+        int rev = 0;
+        while(n>0){
+            rev = rev*10 + (n%10);
+            n /= 10;
+        }
+        return rev;
+    }
+    public boolean sumOfNumberAndReverse(int num) {
+        for(int i=0;i<=num;i++){
+            if(i+reverse(i) == num) return true;
+        }
+        return false;
+    }
+}
