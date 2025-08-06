ac63853a0e58c86247d330d4bc69a15ae2e5ff4f
B-Madhumitha <madhubgoud20@gmail.com>
2025-08-06 09:30:14 +0530

    Time: 2 ms (37.00%) | Memory: 42.1 MB (25.46%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.equals(word.toLowerCase())) return true;
+        else if(word.equals(word.toUpperCase())) return true;
+        else if(word.equals(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())) return true;
+        else return false;
+    }
+}
