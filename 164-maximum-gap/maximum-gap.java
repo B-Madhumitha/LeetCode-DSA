bdf3a9f7c04ba06604a6ab31812e7078e8fa5451
B-Madhumitha <madhubgoud20@gmail.com>
2025-09-01 20:00:53 +0530

    Time: 41 ms (34.79%) | Memory: 60.8 MB (63.55%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length<2) return 0;
+        Arrays.sort(nums);
+        int maxGap = 0;
+        for(int i=0;i<nums.length-1;i++){
+            int diff = nums[i+1]-nums[i];
+            maxGap = Math.max(diff,maxGap);
+        }
+        return maxGap;
+    }
+}
