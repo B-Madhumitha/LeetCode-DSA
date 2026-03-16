5ec8ea9fad06640a6bef4a20c835ac5e889c5f77
B-Madhumitha <madhubgoud20@gmail.com>
2026-03-16 18:43:45 +0530

    Time: 71 ms (10.24%) | Memory: 125.4 MB (77.95%) - LeetSync

---
+++ 3779-eat-pizzas/eat-pizzas.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long maxWeight(int[] pizzas) {
+        long ans = 0, totalDays = pizzas.length/4, oddDays = (totalDays + 1)/2;
+        Arrays.sort(pizzas);
+        for (int i = pizzas.length-1, day = 1; day <= totalDays; --i, ++day) {
+            if (day > oddDays) --i;
+            ans += (long)pizzas[i];
+        }
+        return ans;
+    }
+}
