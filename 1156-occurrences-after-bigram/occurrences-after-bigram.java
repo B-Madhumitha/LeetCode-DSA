58e0bc145d07f4a1be8acf6fd51201972481af98
B-Madhumitha <madhubgoud20@gmail.com>
2026-04-02 22:05:17 +0530

    Time: 1 ms (30.62%) | Memory: 42.8 MB (89.00%) - LeetSync

---
+++ 1156-occurrences-after-bigram/occurrences-after-bigram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] words = text.split(" ");
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<words.length-2;i++){
+            String f = words[i];
+            String s = words[i+1];
+            if(f.equals(first) && s.equals(second)) list.add(words[i+2]);
+        }
+        return list.toArray(new String[0]);
+    }
+}
